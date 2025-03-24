ed92c14a0a928973f242817cb630af0e0e3d0f47
lianaaydinyan <lian555ayd@gmail.com>
2025-03-25 01:14:21 +0400

    ))

--- CharDriver.c
+++ CharDriver.c
@@ -36,7 +36,7 @@ static ssize_t loop_write(struct file* filep, const char __user* buffer, size_t 
     char* kernel_buffer;
 
     // Allocate buffer to store user data
-    kernel_buffer = kvmalloc(len);
+    kernel_buffer = kvmalloc(len, GFP_KERNEL);
     if (!kernel_buffer)
     {
         printk(KERN_ERR "loop: Failed to allocate memory\n");
